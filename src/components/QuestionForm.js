4d92d59816df71ae5a47f912d8d1d86bfe36cf91
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-07-19 22:30:37 +0300

    Handling submit form function

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -19,8 +19,37 @@ function QuestionForm(props) {
 
   function handleSubmit(event) {
     event.preventDefault();
-    console.log(formData);
+    const questionData = {
+      prompt: formData.prompt,
+      answers: [
+        formData.answer1,
+        formData.answer2,
+        formData.answer3,
+        formData.answer4,
+      ],
+      correctIndex: formData.correctIndex,
+    };
+    fetch("http://localhost:4000/questions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(questionData),
+    })
+      .then(res => res.json())
+      .then(() => questionData);
   }
+      //   props.onAddQuestion(data);
+      //   setFormData({
+      //     prompt: "",
+      //     answer1: "",
+      //     answer2: "",
+      //     answer3: "",
+      //     answer4: "",
+      //     correctIndex: 0,
+      //   });
+      // }
+  // }
 
   return (
     <section>
