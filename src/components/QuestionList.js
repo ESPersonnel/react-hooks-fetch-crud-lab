bac2d6516ee806f97dc59446f5f92dbba573d3c0
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-07-19 18:05:41 +0300

    Added GET request

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then(res => res.json())
+      .then(data => setQuestions(data))
+  }, [])
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{/* display QuestionItem components here after fetching */}
+        <QuestionItem question={questions[0]} />
+      </ul>
     </section>
   );
 }
