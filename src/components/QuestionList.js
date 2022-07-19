cd795a5351e886e612bd83eb3fd20bd83387a150
Wycliffe Ndiba <w.ndiba@gmail.com>
2022-07-19 22:34:10 +0300

    added functions for delete, change & calling QuestionItem

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -11,11 +11,46 @@ function QuestionList() {
       .then(data => setQuestions(data))
   }, [])
 
+
+  function handleDelete(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE"
+    })
+      .then(res => res.json())
+      .then(() => {
+        const updatedQuestions = questions.filter((q) => q.id !== id);
+        setQuestions(updatedQuestions);
+      });
+  }
+
+  function handleChange(id, correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        correctIndex
+      })
+    })
+      .then(res => res.json())
+      .then(data => setQuestions(questions.map(question => question.id === id ? data : question)))
+  }
+
+  const questionItems = questions.map(question => (
+    <QuestionItem
+      key={question.id}
+      question={question}
+      onDeleteClick={handleDelete}
+      onChange={handleChange}
+    />
+  ))
+
   return (
     <section>
       <h1>Quiz Questions</h1>
       <ul>{/* display QuestionItem components here after fetching */}
-        <QuestionItem question={questions[0]} />
+        {questionItems}
       </ul>
     </section>
   );
